e7ca9e44d243e5ac126d35a018253da24437c4d7
Dávid Huljak <david.huljak@gmail.com>
2024-03-29 12:54:16 +0100

    improvements, bug fixes

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -4,6 +4,23 @@ import { Link } from "react-router-dom";
 const HomeComponent = () => {
   return (
     <div>
+      <div className="info-wrapper">
+        <div className="info">
+          Studenti Gymnázia Paměti národa a společnost Rekola vám s potěšením
+          oznamují nově vzniklý historicky vzdělávací projekt;{" "}
+          <strong>Pamětníci na Rekolech </strong>. Tato nová iniciativa studentů
+          3. ročníku gymnázia spojuje vzdělávání a poznávání kulturního i
+          historického aspektu naší krásné země s cílem připomenout si
+          pamětníky, kteří stále odehrávají klíčovou roli v naší historii.
+        </div>
+        <a
+          href="https://gymnazium.pametnaroda.cz/2024/03/27/pametnici-na-rekolech/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Čtěte více na stránkách Gymnázia Paměti národa
+        </a>
+      </div>
       <div className="card-wrapper">
         {data.map((person, index) => (
           <div key={index} className="card">
