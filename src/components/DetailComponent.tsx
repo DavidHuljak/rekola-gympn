cc4d7d0ae060bb915727a8e59b1248fe6ab4b6a6
Dávid Huljak <david.huljak@gmail.com>
2024-08-31 21:58:17 +0200

    bug fixes

--- src/components/DetailComponent.tsx
+++ src/components/DetailComponent.tsx
@@ -15,7 +15,20 @@ const DetailComponent = ({
 }: DetailComponentProps) => {
   return (
     <div className="info-wrapper">
-      <h2>{name}</h2>
+      <h2
+        onDoubleClick={() => {
+          if (name.includes("Kuna")) {
+            window.open(
+              "https://suno.com/song/f47208ad-44c7-4d95-92ca-8c28faa04bfb"
+            );
+          }
+          if (name.includes("Lasica")) {
+            window.open("https://youtu.be/3Ha8WDmNdpU?si=c4ok6CDpNlaPSzIV");
+          }
+        }}
+      >
+        {name}
+      </h2>
       <h3>
         {birth} {death}
       </h3>
