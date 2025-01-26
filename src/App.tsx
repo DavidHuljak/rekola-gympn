6878a165aefdbe9288370b448f3bb0f3cd067e3c
Dávid Huljak <david.huljak@gmail.com>
2025-01-27 00:03:17 +0100

    updated api, bug fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,101 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import data from "./assets/data.json";
 import DetailComponent from "./components/DetailComponent";
 import HomeComponent from "./components/HomeComponent";
 import FooterComponent from "./components/FooterComponent";
 import HeaderComponent from "./components/HeaderComponent";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-const App = () => (
-  <div className="main-wrapper">
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <HeaderComponent />
-              <HomeComponent />
-              <FooterComponent />{" "}
-            </>
-          }
-        />
-        {data.map((person, index) => (
+interface Person {
+  name: string;
+  birth: string;
+  death: string;
+  text: string;
+  link: string;
+  path: string;
+}
+
+const App = () => {
+  const [data, setData] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get<Person[]>(
+          "https://api.huljak.cz/school/rekola-project"
+        );
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="main-wrapper">
+        <BrowserRouter>
+          <HeaderComponent />
+          <h2 className="loader">Načítání...</h2>
+          <FooterComponent />
+        </BrowserRouter>
+      </div>
+    );
+  }
+  return (
+    <div className="main-wrapper">
+      <BrowserRouter>
+        <Routes>
           <Route
-            key={index}
-            path={`/${person.path}`}
+            path="/"
             element={
               <>
                 <HeaderComponent />
-                <DetailComponent
-                  name={person.name}
-                  birth={"* " + person.birth}
-                  death={person.death.length <= 0 ? "" : "† " + person.death}
-                  text={person.text}
-                  link={person.link}
-                />
+                <HomeComponent data={data} />
+                <FooterComponent />{" "}
+              </>
+            }
+          />
+          {data.map((person, index) => (
+            <Route
+              key={index}
+              path={`/${person.path}`}
+              element={
+                <>
+                  <HeaderComponent />
+                  <DetailComponent
+                    name={person.name}
+                    birth={"* " + person.birth}
+                    death={person.death.length <= 0 ? "" : "† " + person.death}
+                    text={person.text}
+                    link={person.link}
+                  />
+                  <FooterComponent />
+                </>
+              }
+            />
+          ))}
+          <Route
+            path="*"
+            element={
+              <>
+                <HeaderComponent />
+                <h2 className="not-found">Stránka nenalezena.</h2>
+                <Link to="/" replace />
                 <FooterComponent />
               </>
             }
           />
-        ))}
-        <Route
-          path="*"
-          element={
-            <>
-              <HeaderComponent />
-              <h2 className="not-found">Stránka nenalezena.</h2>
-              <Link to="/" replace />
-              <FooterComponent />
-            </>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,28 +1,41 @@
-import data from "../assets/data.json";
 import { Link } from "react-router-dom";
 
-const HomeComponent = () => {
+interface Person {
+  name: string;
+  birth: string;
+  death: string;
+  text: string;
+  link: string;
+  path: string;
+}
+
+const HomeComponent: React.FC<{ data: Person[] }> = ({ data }) => {
   return (
     <div>
       <div className="info-wrapper">
         <div className="info">
           Studenti Gymnázia Paměti národa a společnost Rekola vám s potěšením
           oznamují nově vzniklý historicky vzdělávací projekt;{" "}
-          <strong>Pamětníci na Rekolech </strong>. Tato nová iniciativa studentů
+          <strong>Pamětníci na Rekolech</strong>. Tato nová iniciativa studentů
           3. ročníku gymnázia spojuje vzdělávání a poznávání kulturního i
           historického aspektu naší krásné země s cílem připomenout si
           pamětníky, kteří stále odehrávají klíčovou roli v naší historii.
         </div>
         <a
-          href="https://gymnazium.pametnaroda.cz/2024/03/27/pametnici-na-rekolech/"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            alert(
+              "Z důvodu osobní nepohody ředitelky školy byl tento projekt z její strany zakázán."
+            )
+          }
         >
           Čtěte více na stránkách Gymnázia Paměti národa
         </a>
       </div>
       <div className="card-wrapper">
-        {data.map((person, index) => (
+        {data.map((person: Person, index: number) => (
           <div key={index} className="card">
             <Link to={`/${person.path}`} className="card-link">
               {person.name}
